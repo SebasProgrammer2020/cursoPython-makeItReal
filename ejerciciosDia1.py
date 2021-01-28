f6f1e2c7f8a64ed43d0a60c54f2feb005d67f702
SEBASTIÁN LONDOÑO <sebastian.snlo@gmail.com>
2021-01-28 05:46:20 +0000

    solucion ejercicios 1,2,3

--- ejerciciosDia1.py
+++ ejerciciosDia1.py
@@ -1,4 +1,18 @@
 # Escribe un programa que le pida al usuario ingresar una frase y la imprima en la consola
 print("Enter a phrase: ")
 phrase = input()
-print("Your phrase is:" + phrase)
+print("Your phrase is: " + phrase)
+
+# Escribe un programa que le pregunte al usuario su nombre e imprima "Hola " seguido del nombre y un signo de exclamaciónk
+print("Enter your name: ")
+name = input()
+print("Hi " + name + " !")
+
+# Escribe un programa que le pida al usuario ingresar un número, luego le pida un segundo número, e imprima en la consola la suma de los dos números que ingresó el usuario.
+print("Enter the first number: ")
+firstNumber = int(input())
+print("Enter the second number: ")
+secondNumber = int(input())
+print("The sum of your numbers are: " + str(firstNumber + secondNumber))
+
+
--- main.py
+++ main.py
@@ -1,28 +1,32 @@
-# hola mundo en python3
-print("hola mundo")
-print()
+# # hola mundo en python3
+# print("hola mundo")
+# print()
 
-# input example, uncomment to execute
-# print("Ingrese su nombre ")
-# x = input()
-# print("hola " + x)
+# # input example, uncomment to execute
+# # print("Ingrese su nombre ")
+# # x = input()
+# # print("hola " + x)
 
-# escamapando caracteres
-print("hola \"mundo\"")
-'''comentarios 
-de multiples linesas
-en python'''
+# # escamapando caracteres
+# print("hola \"mundo\"")
+# '''comentarios
+# de multiples linesas
+# en python'''
 
-# exponentes
-# con + no se puede concatenar variables y cadenas
-print()
-print("Exponentes: ", 15**2)
+# # exponentes
+# # con + no se puede concatenar variables y cadenas
+# print()
+# print("Exponentes: ", 15**2)
 
-# castear numeros a cadenas
-print()
-print("castear numeros a enteros: " + "edad " + str(26))
+# # castear numeros a cadenas
+# print()
+# print("castear numeros a enteros: " + "edad " + str(26))
 
-# string multilinea
-print()
-message = """hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python"""
-print(message)
+# # string multilinea
+# print()
+# message = """hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python"""
+# print(message)
+
+x=int(input("ingrese primer numero "))
+y=int(input("ingrese segundo numero "))
+print("La suma de los numeros es ", (x+y))
