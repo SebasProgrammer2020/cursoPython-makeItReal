d3e508532695375243bf1d387ae2089ff0249733
SEBASTIÁN LONDOÑO <sebastian.snlo@gmail.com>
2021-01-28 05:18:23 +0000

    Initial commit

---
+++ ejerciciosDia1.py
@@ -0,0 +1,4 @@
+# Escribe un programa que le pida al usuario ingresar una frase y la imprima en la consola
+print("Enter a phrase: ")
+phrase = input()
+print("Your phrase is:" + phrase)
---
+++ main.py
@@ -0,0 +1,28 @@
+# hola mundo en python3
+print("hola mundo")
+print()
+
+# input example, uncomment to execute
+# print("Ingrese su nombre ")
+# x = input()
+# print("hola " + x)
+
+# escamapando caracteres
+print("hola \"mundo\"")
+'''comentarios 
+de multiples linesas
+en python'''
+
+# exponentes
+# con + no se puede concatenar variables y cadenas
+print()
+print("Exponentes: ", 15**2)
+
+# castear numeros a cadenas
+print()
+print("castear numeros a enteros: " + "edad " + str(26))
+
+# string multilinea
+print()
+message = """hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python hola estoy mejorando mis hablidades en python"""
+print(message)
